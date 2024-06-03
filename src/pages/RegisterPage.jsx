5856e2a76e133f0070812c0162cc1ab1cc63f51e
AminebajjiDEV <aminebajji34@gmail.com>
2024-06-03 13:15:27 +0100

    Second Commit

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,64 +1,52 @@
 import React from 'react'
-
+import "../partials/RegisterPage.scss"
 const RegisterPage = () => {
-    return (
-        <div class="container">
-    <div class="title">Registration</div>
-    <div class="content">
-      <form action="#">
-        <div class="user-details">
-          <div class="input-box">
-            <span class="details">Full Name</span>
-            <input type="text" placeholder="Enter your name" required></input>
+  return (
+    <div class="register_container">
+      <div class="title">Registration</div>
+      <div class="register_content">
+        <form action="#">
+          <div class="user-details">
+            <div class="input-box">{/* Last Name */}
+              <span class="details">Last Name</span>
+              <input type="text" placeholder="Enter your last name" required></input>
+            </div>
+            <div class="input-box">{/* First Name */}
+              <span class="details">First Name</span>
+              <input type="text" placeholder="Enter your first name" required></input>
+            </div>
+            <div class="input-box">{/* Email */}
+              <span class="details">Email</span>
+              <input type="text" placeholder="Enter your email" required></input>
+            </div>
+            <div class="input-box">{/* Phone Number */}
+              <span class="details">Phone Number</span>
+              <input type="tel" placeholder="Enter your number" required></input>
+            </div>
+            <div class="input-box">{/* Password */}
+              <span class="details">Password</span>
+              <input type="password" placeholder="Enter your password" required></input>
+            </div>
+            <div class="input-box">{/* Confirm Password */}
+              <span class="details">Confirm Password</span>
+              <input type="password" placeholder="Confirm your password" required></input>
+            </div>
           </div>
-          <div class="input-box">
-            <span class="details">Username</span>
-            <input type="text" placeholder="Enter your username" required></input>
-          </div>
-          <div class="input-box">
-            <span class="details">Email</span>
-            <input type="text" placeholder="Enter your email" required></input>
-          </div>
-          <div class="input-box">
-            <span class="details">Phone Number</span>
-            <input type="text" placeholder="Enter your number" required></input>
-          </div>
-          <div class="input-box">
-            <span class="details">Password</span>
-            <input type="text" placeholder="Enter your password" required></input>
-          </div>
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required></input>
-          </div>
-        </div>
-        <div class="gender-details">
-          <input type="radio" name="gender" id="dot-1"></input>
-          <input type="radio" name="gender" id="dot-2"></input>
-          <input type="radio" name="gender" id="dot-3"></input>
-          <span class="gender-title">Gender</span>
-          <div class="category">
-            <label for="dot-1">
-            <span class="dot one"></span>
-            <span class="gender">Male</span>
-          </label>
-          <label for="dot-2">
-            <span class="dot two"></span>
-            <span class="gender">Female</span>
-          </label>
-          <label for="dot-3">
-            <span class="dot three"></span>
-            <span class="gender">Prefer not to say</span>
+          <div className="profile-picture">{/* profile-picture */}
+            <input id='image' type="file" name='profilePicture' accept='image/*' style={{ display: "none" }} required />
+            <label htmlFor="image">
+              <p>Upload your Photo here</p>
+              <img src="/assets/upload-image.png" alt="add profile picture" />
             </label>
           </div>
-        </div>
-        <div class="button">
-          <input type="submit" value="Register"></input>
-        </div>
-      </form>
+          <div class="button">
+            <input type="submit" value="Register"></input>
+          </div>
+        </form>
+        <span className='already-signedUP'>Already have an account? <a href="/login">log in here</a></span>
+      </div>
     </div>
-  </div>
-    )
+  )
 }
 
 export default RegisterPage
